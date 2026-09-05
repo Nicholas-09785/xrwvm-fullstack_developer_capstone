ed677fa8bf625b7fbfd8261346583f869b9e63cc
Nicholas-09785 <npetrelli@live.ca>
2026-09-05 01:28:41 -0400

    Added login, logout, and registration functionality

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,17 +1,25 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
+from django.views.generic import TemplateView
 
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
+    path(route='register', view=views.registration, name='register'),
 
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
+    path(route='login', view=views.login_user, name='login'),
+
+    # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
+    # path('login/', TemplateView.as_view(template_name="index.html")),
+
+
 
     # path for add a review view
 
